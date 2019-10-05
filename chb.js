b5c670f97ad150457499ab67944e94ed93db758b
Chris Hansen <christopher.hansen@hp.com>
2019-10-04 21:37:57 -0500

    Added a few questions and updated logic to include help text and only answer if it's not the bot

--- chb.js
+++ chb.js
@@ -9,12 +9,23 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   var message = msg.content;
-  questions.questions.forEach(question => {
-    if (message.toLowerCase().includes(question.question.toLowerCase())) {
-      msg.reply(question.answer);
-      return;
-    }
-  });
+  if (message.toLowerCase().includes("help questions")) {
+    var questionsText = "Here is a list of questions I can answer:"
+    questions.questions.forEach(question => {
+      questionsText += "\nhelp " + question.question
+    });
+    msg.reply(questionsText);
+    return;
+  }
+
+  if (msg.author.username !== client.user.username) {
+    questions.questions.forEach(question => {
+      if (message.toLowerCase().includes("help " + question.question.toLowerCase())) {
+        msg.reply(question.answer);
+        return;
+      }
+    });
+  }
 });
 
 client.login(auth.token);
