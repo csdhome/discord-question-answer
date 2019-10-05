19a557baa14a82412093acded09657815b31e255
Chris Hansen <christopher.hansen@hp.com>
2019-10-04 20:55:20 -0500

    Updated questions import

--- dqa.js
+++ dqa.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
-
+const questions = require('./questions.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  questions = require('./questions.json');
   var message = msg.content;
   questions.questions.forEach(question => {
     if (message.toLowerCase().includes(question.question.toLowerCase())) {
